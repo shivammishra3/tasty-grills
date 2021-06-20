bbdd90885f035ffeb8579e322a0551ee9384de90
Shivam Mishra <mshiv619@gmail.com>
2021-06-20 22:32:39 +0530

    updated docs and styles

--- src/App.js
+++ src/App.js
@@ -1,126 +1,180 @@
-import React, { useState } from "react";
+import React, {
+  useState
+} from "react";
 import './style.css';
 
 var foodDB = {
-  NorthIndian: [
-    { name: "Matar Paneer & Butter Nan", rating: "Rating: 4/5" },
-    { name: "Veg Mixed Shahi Korma", rating: "Rating: 3.5/5" },
-    { name: "Onion Kulcha", rating: "Rating: 3/5" },
-    { name: "Chola Bhatura", rating: "Rating: 3/5" }
+  NorthIndian: [{
+      name: "Chola Bhatura",
+      rating: "Rating: 5/5"
+    },
+    {
+      name: "Dal Bati Churma",
+      rating: "Rating: 4.0/5"
+    },
+    {
+      name: "Amritsari Kulcha",
+      rating: "Rating: 4.0/5"
+    },
+    {
+      name: "Matar Paneer & Laccha Paratha",
+      rating: "Rating: 4.0/5"
+    }
   ],
 
-  Bengali: [
-    {
-      name: "Chicken Biryani",
-      rating: "5/5"
+  Bengali: [{
+      name: "Rossogola",
+      rating: "Rating: 5/5"
     },
     {
       name: "Luchi-Alur Dom",
-      rating: "4.5/5"
+      rating: "Rating: 4.0/5"
+    },
+    {
+      name: "Chicken Biryani",
+      rating: "Rating: 4.0/5"
     },
     {
       name: "Kosha Mangsho",
-      rating: "4.5/5"
+      rating: "Rating: 3.5/5"
     },
-    {
-      name: "Rossogola",
-      rating: "4.5/5"
-    }
+
   ],
-  Chinese: [
-    {
+  Chinese: [{
       name: "Paneer Manchurian",
-      rating: "3.5/5"
+      rating: "Rating: 3.5/5"
     },
     {
       name: "Chicken Chilli Garlic Chowmein",
-      rating: "5/5"
+      rating: "Rating: 3.0/5"
     },
     {
       name: "Pan Fried Chicken Cheese Momo",
-      rating: "5/5"
+      rating: "Rating: 4.0/5"
     },
     {
       name: "Hot Schezwan Noodles",
-      rating: "4/5"
+      rating: "Rating: 4.0/5"
     }
   ],
-  Desserts: [
-    {
+  Desserts: [{
       name: "BadamPista Kulfi",
-      rating: "3.5/5"
+      rating: "Rating: 5/5"
     },
     {
       name: "ButterScotch Icecream",
-      rating: "5/5"
+      rating: "Rating: 4.5/5"
     },
     {
       name: "Chocolate Pudding",
-      rating: "5/5"
+      rating: "Rating: 4.0/5"
     },
     {
       name: "Vanilla Cake",
-      rating: "4/5"
+      rating: "Rating: 4.5/5"
     }
   ]
 };
 
 export default function App() {
-  const [selectedCuisine, setCuisine] = useState("Chinese");
+  var [selectedCuisine, setCuisine] = useState("NorthIndian");
+
   function cuisineClickHandler(cuisine) {
     setCuisine(cuisine);
   }
-  return (
-    <div className="App">
-      <h1> 🥘 Tasty Grills </h1>
-      <p style={{ fontSize: "smaller" }}>
-        {" "}
-        Checkout my favorite dishes. Select a cuisine to get started{" "}
-      </p>
+  return ( <
+    div className = "App" >
+    <
+    h1 > 🥘Tasty Grills < /h1> <
+    p style = {
+      {
+        fontSize: "medium",
+        fontFamily: "'Rubik', sans-serif"
+      }
+    } > {
+      " "
+    }
+    Checkout my favorite dishes. Select a cuisine to get started {
+      " "
+    } <
+    /p>
 
-      <div>
-        {Object.keys(foodDB).map(function (cuisine) {
-          return (
-          <button
-            onClick={() => cuisineClickHandler(cuisine)}
-            style={{
+    <
+    div > {
+      Object.keys(foodDB).map(function (cuisine) {
+        return ( <
+          button onClick = {
+            () => cuisineClickHandler(cuisine)
+          }
+          style = {
+            {
               cursor: "pointer",
-              background: "#E5E7EB",
-              borderRadius: "0.5rem",
+              background: "#FFFFFF",
+              borderRadius: "1.5rem",
               padding: "0.5rem  1rem",
               border: "1px solid black",
-              margin: "1rem 0.3rem"
-            }}
-          >
-            {cuisine}
-          </button>
-          );
-        })}
-      </div>
-      <hr />
-      <div style={{ textAlign: "left" }}>
-        <ul style={{ paddingInlineStart: "0" }}>
-          {foodDB[selectedCuisine].map(function(food) {
-            return (
-            <li
-              key={food.name}
-              style={{
-                listStyle: "none",
-                padding: "1rem",
-                border: "1px solid #D1D5DB",
-                width: "70%",
-                margin: "1rem 0rem",
-                borderRadius: "2rem"
-              }}
-            >
-              {" "}
-              <div style={{ fontSize: "larger" }}> {food.name} </div>
-              <div style={{ fontSize: "smaller" }}> {food.rating} </div>
-            </li>
-            );
-          })}
-        </ul>
-      </div>
-    </div>
+              margin: "1rem 0.3rem",
+              fontFamily: "'Rubik', sans-serif",
+            }
+          } > {
+            cuisine
+          } <
+          /button>
+        );
+      })
+    } <
+    /div> 
+    <hr/>
+    <div style = {
+      {
+        textAlign: "left"
+      }
+    } >
+    < ul style = {
+      {
+        paddingInlineStart: "0"
+      }
+    } > {
+      foodDB[selectedCuisine].map(function (food) {
+        return ( <
+          li id = "list"
+          key = {
+            food.name
+          }
+          style = {
+            {
+              listStyle: "none",
+              padding: "1rem",
+              border: "2px solid #FFFFFF",
+              width: "55%",
+              margin: "1rem 0rem",
+              borderRadius: "2rem",
+              fontFamily: "'Mate SC', serif",
+              textAlign: "left",
+              display: "block",
+              marginLeft: "auto",
+              marginRight: "auto",
+            }
+          } > {
+            " "
+          } <
+          div style = {
+            {
+              fontSize: "larger"
+            }
+          } > {
+            food.name
+          } < /div> <
+          div style = {
+            {
+              fontSize: "smaller"
+            }
+          } > {
+            food.rating
+          } < /div> < /li >
+        );
+      })
+    } </ul> < /div > <
+    /div>
   );
 }
